713c1838c23d27c946a55d5d06fb35baed0e3249
Piotr Heinzeman <piotr@i-zn.pl>
2020-10-03 01:54:05 +0200

    add: Spring Initializr files

---
+++ src/main/java/com/heinzelman/lipinski/LipinskiApplication.java
@@ -0,0 +1,13 @@
+package com.heinzelman.lipinski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LipinskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LipinskiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heinzelman/lipinski/LipinskiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heinzelman.lipinski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LipinskiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
